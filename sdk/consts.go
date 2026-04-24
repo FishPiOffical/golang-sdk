85c252f006ef9b8b341b6d62a90b268f6e55c44b
fghwett <1058178245@qq.com>
2026-04-24 16:16:28 +0800

    版本更新

--- sdk/consts.go
+++ sdk/consts.go
@@ -1,7 +1,7 @@
 package sdk
 
 const (
-	VERSION = "Golang/v0.0.13"
+	VERSION = "Golang/v0.0.14"
 
 	BaseUrl   = "https://fishpi.cn"
 	UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)"
